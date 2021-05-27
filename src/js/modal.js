1f0a1bd29cb67963565e9e282028e34c13ca9daa
Olexiy-repin <repinlyoha@gmail.com>
2021-05-27 19:13:17 +0300

    done

---
+++ src/js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  console.log(refs.openModalBtn);
+
+  refs.openModalBtn.forEach(function (btn) {
+    btn.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
